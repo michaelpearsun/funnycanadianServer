24a6cde2866251ed41140ad2d1a07ecf1d38a566
Michael Pearson <michaelpearsun@gmail.com>
2021-01-19 15:13:13 -0600

    Intro to Express

--- server.js
+++ server.js
@@ -1,46 +1,20 @@
-const fs = require('fs');
-const http = require('http');
-const path = require('path');
+const express = require('express');
+const morgan = require('morgan')
 
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    console.log(`Request for ${req.url} by method ${req.method}`);
+const app = express();
+app.use(morgan('dev'));
 
-    if (req.method === 'GET') {
-        let fileUrl = req.url;
-        if (fileUrl ==='/') {
-            fileUrl = '/index.html';
-        }
+app.use(express.static(__dirname + '/public'))
 
-        const filePath = path.resolve('./public' + fileUrl);
-        const fileExt = path.extname(filePath);
-        if (fileExt === '.html') {
-            fs.access(filePath, err => {
-                if (err) {
-                    res.status = 404;
-                    res.setHeader('Content-Type', 'text/html');
-                    res.end(`<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>`);
-                    return;
-                }
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/html');
-
-                fs.createReadStream(filePath).pipe(res)
-            })
-        } else {
-            res.status = 404;
-            res.setHeader('Content-Type', 'text/html');
-            res.end(`<html><body><h1>Error 404: ${fileUrl} is not an HTML file</h1></body></html>`);
-        }
-    } else {
-        res.status = 404;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(`<html><body><h1>Error 404: ${req.method} not supported</h1></body></html>`);
-    }
+app.use((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 });
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
